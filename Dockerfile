fb133a795bbf99ee1f8cd0fbf4a145006ba57d45
arbiva <arbiva@post.bgu.ac.il>
2022-07-05 15:12:56 +0300

    pandas

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,19 @@
+# Specify base image(+tag):
 FROM python:3.9
-
+# Add file into container:
 ADD main.py .
-
+# Install dependencies:
 RUN pip install requests pandas multitasking numpy yfinance pymongo yahoofinancials
-
+# The command to execute when the container is started:
 CMD ["python", "./main.py"]
 
+## docker -v
+## build docker image: docker -t python-imdb
+## docker run pythom-imdb
 
 
-#FROM python:3.9-alpine3.15
+#### Alternative:
+#FROM python:3.9-alpine3.15 (the slim version for saving space)
 #WORKDIR /app
 #COPY ./requirements.txt .
 #RUN pip install -r requirements.txt
--- main.py
+++ main.py
@@ -5,12 +5,13 @@ import multitasking
 import datetime
 #from pandas_datareader import data as pdr
 import yfinance as yf
+yf.pdr_override()
 import pymongo as pym
 from pymongo import MongoClient
 from yahoofinancials import YahooFinancials
 
 
-cluster = 'mongodb+srv://arbiva:Adi101010@cluster0.86nym.mongodb.net/test?retryWrites=true&w=majority'
+cluster = 'mongodb+srv://?????@cluster0.86nym.mongodb.net/test?retryWrites=true&w=majority'
 client = MongoClient(cluster)
 print(client.list_database_names())
 ### Accessing a database:
@@ -63,13 +64,23 @@ data = db.data
 
 symbol = 'TSLA'
 ticker = yf.Ticker(symbol)
-print(ticker.history())
+#print(ticker.history())
 end_time = datetime.datetime.now()
 start_time = end_time - datetime.timedelta(days=365)
-tickerdata = ticker.history(start=start_time, end=end_time)
+tickerdata = ticker.history(start=retrievedate, end=end_time)
+print('*')
 print(tickerdata)
-print(end_time)
-print(start_time)
+print('*')
+### The schema according to which new document are being build:
+dataoflists = {'Date': [datetime.datetime.now()], 'Open': [0], 'High': [1], 'Low': [1], 'Close': [1], 'AdjClose': [1], 'Volume': [1], 'Project_id': [symbol]}
+### Inserting a new document following every search of symbol by client:
+x = col.insert_one(dataoflists)
+df = pd.DataFrame(dataoflists)
+print(df)
+
+#print(tickerdata)
+#print(end_time)
+#print(start_time)
 #temp = datetime.timedelta('start_time', 'end_time')
 # df = pd.DataFrame({"tickerdata"})
 # df
@@ -78,12 +89,16 @@ print(start_time)
 #filtering for last date in sort results:
 #firstquary = db.data.findOne({'EndDate'})
 for x in db.data.find():
-#findsymbol = db['tset'].find({"symbol": "TSLA"}).count()
     if db.data.count_documents({"symbol": "TSLA"})==0:
 #if db.data.count_documents({'symbol': "TSLA"}, limit=1) == 0:
-        print("4")
+        print("DataFrame.empty")
 else:
     print(ticker.history(start=retrievedate, end=end_time))
+
+
+
+
+
 #else:data.find().sort("EndDate", -1)
 ## retrieve between_dates()
 # symbol_df = yf.download('symbol',
@@ -91,9 +106,6 @@ else:
 #                       end=firstquary,
 #                       progress=False,)
 # print(symbol_df.head())\]
-
-
-
 ### Creating a database
 # mydb = client["test"]
 #creating a collection
@@ -103,20 +115,13 @@ else:
 # end = dt.datetime.now()
 # start = end - dt.timedelta(days=365)
 # start, end
-
-
-
-
 #Creating a Ticker obgect - this object suppose to get what the client is inputing as 'SYMBOL'
 # symbol = 'TSLA'
 # ticker = yf.Ticker(symbol)
 #When we whant to view stock history (can get parameters as "max" and so on)
 # print(ticker.history())
-
-
 # database.create_collection("data")
 # client = pymongo.MongoClient('mongodb://localhost:27017')
-
 # tsla_df = yf.download('TSLA',
 #                       start='2019-01-01',
 #                       end='2021-06-12',
